e79660aa2463a1de8c542e03dadeea53b206730f
Timo Früh <dev@tifrueh.ch>
2026-07-07 18:18:18 +0200

    Add template man page

---
+++ scripts/docgen.py
@@ -0,0 +1,29 @@
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+import subprocess
+from pathlib import Path
+from datetime import datetime
+
+class DocGenBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+
+        md_path = Path(self.root + "/Documentation/md2anki.1.md")
+        man_path = Path(self.root + "/Documentation/md2anki.1")
+        today = datetime.today().strftime('%Y-%m-%d')
+
+        subprocess.run(
+            [
+                "pandoc",
+                "--from=markdown",
+                "--standalone",
+                "--variable",
+                f"footer={str(version)}",
+                "--variable",
+                f"date={str(today)}",
+                "--to=man",
+                str(md_path),
+                "-o",
+                str(man_path),
+            ],
+            check=True,
+        )
