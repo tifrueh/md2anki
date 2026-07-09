34724c16f81fa2f21403846a43b1c2de17ab0e6a
Timo Früh <dev@tifrueh.ch>
2026-07-09 09:31:32 +0200

    Improve docgen build hook
    * Convert all Markdown files in the Documentation directory instead of
      just specified ones.
    * Remove built man pages on "hatch clean".

--- scripts/docgen.py
+++ scripts/docgen.py
@@ -5,25 +5,39 @@ from pathlib import Path
 from datetime import datetime
 
 class DocGenBuildHook(BuildHookInterface):
+
     def initialize(self, version, build_data):
 
-        md_path = Path(self.root + "/Documentation/md2anki.1.md")
-        man_path = Path(self.root + "/Documentation/md2anki.1")
+        doc_path = Path(self.root + "/Documentation")
+        md_paths = doc_path.glob("*.md")
         today = datetime.today().strftime('%Y-%m-%d')
 
-        subprocess.run(
-            [
-                "pandoc",
-                "--from=markdown",
-                "--standalone",
-                "--variable",
-                f"footer=v{str(self.metadata.version)}",
-                "--variable",
-                f"date={str(today)}",
-                "--to=man",
-                str(md_path),
-                "-o",
-                str(man_path),
-            ],
-            check=True,
+        for md_path in md_paths:
+
+            man_path = md_path.with_suffix("")
+
+            subprocess.run(
+                [
+                    "pandoc",
+                    "--from=markdown",
+                    "--standalone",
+                    "--variable",
+                    f"footer=v{str(self.metadata.version)}",
+                    "--variable",
+                    f"date={str(today)}",
+                    "--to=man",
+                    str(md_path),
+                    "-o",
+                    str(man_path),
+                ],
+                check=True,
         )
+
+    def clean(self, versions):
+
+        doc_path = Path(self.root + "/Documentation")
+        md_paths = doc_path.glob("*.md")
+
+        for md_path in md_paths:
+            man_path = md_path.with_suffix("")
+            man_path.unlink(missing_ok=True)
